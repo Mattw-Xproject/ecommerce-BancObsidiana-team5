6aa8a09b742c46c73245cc7f77c6e86aa2700763
mattx project <dannygonzalez.exe@gmail.com>
2026-02-09 23:48:18 -0400

    fix: convertir configuraciones de PostCSS y Tailwind a ESM para Vite 7

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,12 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-    content: [
-        "./resources/**/*.blade.php",
-        "./resources/**/*.js",
-        "./resources/**/*.vue",
-        "./app/Http/Livewire/**/*.php",
-        "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
-        "./storage/framework/views/*.php",
-    ],
-    theme: {
-        extend: {},
-    },
-    plugins: [],
+export default {
+  content: [
+    "./resources/**/*.blade.php",
+    "./resources/**/*.js",
+    "./resources/**/*.vue",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
 }
